22c2e8e60938ccc79b42db0dcb7cddfdfcbe74c6
jerytang <jerytang@tencent.com>
2018-03-28 11:07:49 +0800

    同步 haige 的修改

--- src/util_13_constant.js
+++ src/util_13_constant.js
@@ -34,6 +34,8 @@ const grade = {
   6003: '初三',
 };
 
+// 按位 计算
+// 二进制 1-6位，初中 --> 高三，7-12，小学 一到 6年级；
 const deprecatedGrade = {
   1: '初一',
   2: '初二',
@@ -41,6 +43,13 @@ const deprecatedGrade = {
   8: '高一',
   16: '高二',
   32: '高三',
+  64: '一年级',
+  128: '二年级',
+  256: '三年级',
+  512: '四年级',
+  1024: '五年级',
+  2048: '六年级',
+
   3: '初中',
   5: '初中',
   6: '初中',
@@ -50,6 +59,71 @@ const deprecatedGrade = {
   48: '高中',
   56: '高中',
 };
+
+const PRIMARY_GREADE_MAP = {
+  7001: '一',
+  7002: '二',
+  7003: '三',
+  7004: '四',
+  7005: '五',
+  7006: '六',
+};
+
+// 学科标识
+const SING_HIGH = '500';     // 高中
+const SING_JUNIOR = '600';   // 初中
+const SING_PRIMARY = '700';  // 小学
+
+function getGradeName(str) {
+  if (!str) {
+    return '';
+  }
+  const arr = String(str).split(','); // 保存数组
+  if (arr.length === 1) {
+    return Number(arr[0]) > 1000 ? GRADES[arr[0]] :
+           (deprecatedGrade[arr[0]] != undefined ? deprecatedGrade[arr[0]] : '小学');
+  } else {
+    const isHighSchool = str.indexOf(SING_HIGH) > -1 ? 1 : 0;
+    const isMiddSchool = str.indexOf(SING_JUNIOR) > -1 ? 1 : 0;
+    const isPrimarySchool = str.indexOf(SING_PRIMARY) > -1 ? 1 : 0;
+    // 5XXX， 6XXX， 7XXX , 跨年级，就是 全年级
+    const mergeResult = isHighSchool + isMiddSchool + isPrimarySchool;
+    if (mergeResult >= 2) {  // 存在 2个年级，就是全年级, 或者 大于等于 3个年级
+      return '全年级';
+    } else if (arr.length == 2) {
+      let ret = [];
+      let sigalCount = 0; // 大于 14000 ，证明就是 有 小学 年级
+      arr.forEach(key => {
+        ret.push(GRADES[key]);
+        sigalCount += Number(key);
+      });
+
+      if (sigalCount > 14000) { // 大于 14000 就是2个小学年级，小学从 7000 开始
+        ret = [];
+        arr.forEach(key => {
+          ret.push(PRIMARY_GREADE_MAP[key]);
+        });
+        ret = ret.join('/') + '年级';
+      } else {
+        ret = ret.join('/');
+      }
+      return ret;
+    } else if (arr.length == 3) {  // 等于3的情况，这样，判断一个就可以，因为一定是一个年级的。
+      if (arr[0] > 5000 && arr[0] < 6000) { // 高中
+        return '高中';
+      } else if (arr[0] > 6000 && arr[0] < 7000) {
+        return '初中';
+      } else {
+        return '小学';
+      }
+    } else if (arr.length > 3) {  // 当大于3，并且还在一个年级的场景，只有小学存在，扩展使用
+      return '小学';
+    }
+    else {
+      return ['高中', '初中'][mergeResult - 1];
+    }
+  }
+}
 
 const CourseType = {
   // user_all_course 的定义
@@ -64,7 +138,9 @@ const GRADES = {
   '5001,5002,5003,6001,6002,6003': '全年级',
   '6001,6002,6003,5001,5002,5003': '全年级',
   '5001,5002,5003': '高中全部',
+  // '5001,5002,5003': '高一',
   '6001,6002,6003': '初中全部',
+  // '6001,6002,6003': '初一',
   '5001,5002': '高中',
   '5001,5003': '高中',
   '5002,5003': '高中',
@@ -77,6 +153,12 @@ const GRADES = {
   6001: '初一',
   6002: '初二',
   6003: '初三',
+  7001: '一年级',
+  7002: '二年级',
+  7003: '三年级',
+  7004: '四年级',
+  7005: '五年级',
+  7006: '六年级',
 };
 
 mapReverse(GRADES);
