cbc58c4c0ec16dace3391f0d9cb34a881cf91977
jerytang <jerytang@tencent.com>
2018-03-29 16:11:22 +0800

    improve: 生成带版本的 api

---
+++ jsdoc.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+
+const config = require('./package.json');
+
+const { version } = config;
+
+const cmd = `sh ./node_modules/.bin/jsdoc -d ./api/${version} --configure .jsdoc.json --verbose`;
+
+exec(cmd, { env: process.env }, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  if (stderr) {
+    console.log(stderr);
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
+});
